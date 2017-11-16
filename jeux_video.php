a928b77739af2bbd8ca48d98e0ac8c711ed42f0a
Bouchat Marie-Ange <bouchat.marieange@gmail.com>
2017-11-16 12:31:16 +0100

    Backup avant la pause midi

---
+++ jeux_video.php
@@ -0,0 +1,8 @@
+<?php
+$bdd = new PDO('mysql:host=localhost;dbname=Tuto_OpenClassrooms', 'root', 'user');
+// Cette requête sert à faire la connecion en PHP et la base de données mysql
+// Cette requête indique à PHP où se trouve le serveur de base de données MySQL (localhost)
+// Quel est le nom de la base de données MySQL utilisée (Tuto_OpenClassrooms)
+// Le login et le mot de passe pour s'y connecter (ce sont ceux de PHPmyAdmin root - user)
+
+ ?>
